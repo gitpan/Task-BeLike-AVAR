e948bc10f247313d2a7f6fe73a3f735cc57ffba6
Ævar Arnfjörð Bjarmason <avar@cpan.org>
2010-05-21 14:04:26 +0000

    Import of AVAR/Task-BeLike-AVAR-0.01 from CPAN.
    gitpan-cpan-distribution: Task-BeLike-AVAR
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         AVAR/Task-BeLike-AVAR-0.01.tar.gz
    gitpan-cpan-author:       AVAR
    gitpan-cpan-maturity:     released

---
+++ lib/Task/BeLike/AVAR.pm
@@ -0,0 +1,57 @@
+package Task::BeLike::AVAR;
+BEGIN {
+  $Task::BeLike::AVAR::VERSION = '0.01';
+}
+use v5.10.0;
+
+# Useful stuff
+## Moose
+use Any::Moose ();
+use Mouse ();
+use MouseX::Getopt ();
+
+use Moose ();
+use MooseX::Getopt ();
+
+use App::Nopaste ();
+use WWW::Mechanize ();
+use perl5i ();
+
+# My modules
+use App::Nopaste::Service::Linode ();
+use App::QuoteCC ();
+use Bot::Training ();
+use Bot::Training::MegaHAL ();
+use Bot::Training::StarCraft ();
+use Bot::Twatterhose ();
+use Business::IS::PIN ();
+#use Catalyst::Plugin::Upload::Digest ();
+use Crypt::CVS ();
+use Digest::Whirlpool ();
+#use Dist::Zilla::Plugin::MakeMaker::Awesome ();
+#use Dist::Zilla::Plugin::ReadmeFromPod ();
+#use Dist::Zilla::Plugin::VersionFromPrev ();
+use Dist::Zilla::PluginBundle::AVAR ();
+use MediaWiki::USERINFO ();
+#use re::engine::PCRE ();
+#use re::engine::Plan9 ();
+#use Sendmail::PMilter ();
+use Software::License::DWTFYWWI ();
+use Taint::Util ();
+use Task::Bot::Training ();
+use YAML::Syck ();
+## Hailo
+use Hailo ();
+use DBD::mysql ();
+use DBD::Pg ();
+### Tests
+use Test::Expect ();
+use Test::Synopsis ();
+
+1;
+
+=head1 NAME
+
+Task::BeLike::AVAR - Be the most AVAR you can be
+
+=cut
